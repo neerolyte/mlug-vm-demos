b141e05ade49096f460f676b8af936e03db7dee3
David Schoen <dave@lyte.id.au>
2012-09-14 18:58:49 +1000

    adding spree and acng support for gems

--- acng/manifests/base.pp
+++ acng/manifests/base.pp
@@ -1,1 +1,33 @@
-package { apt-cacher-ng: ensure => installed }
+# Install ACNG
+package { apt-cacher-ng: 
+		ensure => installed,
+		alias => 'acng'
+}
+
+# Configure it
+file { "/etc/apt-cacher-ng/acng.conf":
+	ensure => file,
+	force => true,
+	owner => 'root',
+	group => 'root',
+	mode => 0444,
+	source => "/vagrant/manifests/files/acng.conf"
+}
+file { "/etc/apt-cacher-ng/gems_mirrors":
+	ensure => file,
+	force => true,
+	owner => 'root',
+	group => 'root',
+	mode => 0444,
+	source => "/vagrant/manifests/files/gems_mirrors"
+}
+
+# Ensure it runs
+service { 'apt-cacher-ng':
+	ensure => running,
+	require => Package['apt-cacher-ng'],
+	subscribe => [
+		File['/etc/apt-cacher-ng/acng.conf'],
+		File['/etc/apt-cacher-ng/gems_mirrors'],
+	]
+}
